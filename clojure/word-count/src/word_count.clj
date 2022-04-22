907f359f629d4328cba5009d5a5a95981964c8bf
Nunzio Cicone <nunziocicone@gmail.com>
2022-04-22 11:55:05 -0400

    word-count

--- clojure/word-count/src/word_count.clj
+++ clojure/word-count/src/word_count.clj
@@ -1,5 +1,6 @@
-(ns word-count)
+(ns word-count
+  (:require [clojure.string :as str]))
 
-(defn word-count [s] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn word-count [s]
+  (frequencies (re-seq #"\b\w+\b" (str/lower-case s))))
+
